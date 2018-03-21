2a068faeb1bf2a5cfca214a23292f1b4e5b59e87
why2blue <why2blue@gmail.com>
2018-03-21 12:59:20 +0200

    optimize alarm ring

---
+++ Java/src/Subapps/Alarm/MainScreenAlarmer/Main.java
@@ -0,0 +1,23 @@
+package Subapps.Alarm.MainScreenAlarmer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Subapps/Alarm/MainScreenAlarmer/Main.fxml"));
+        primaryStage.setTitle("Logger");
+        primaryStage.setScene(new Scene(root, 260, 220));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
